7e09901efb4c2545cfa3e1c0c9b3a1be79c1c9b1
Pierre Jacquier <pierrejacquier39@gmail.com>
2019-10-10 16:08:50 -0400

    Added bar to std

--- 2d-ackley/ackleyutils.py
+++ 2d-ackley/ackleyutils.py
@@ -95,7 +95,7 @@ def plot_results(U_h, U_h_pred=None,
 
     mean_levels = list(range(2, 15))
     std_levels = np.arange(5, 20) * 0.1
-    fig = plt.figure(figsize=figsize(3.0, 1.5))
+    fig = plt.figure(figsize=figsize(1., 3))
 
     n_plot_x = 2
     n_plot_y = 3
