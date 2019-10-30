b900a535858d58db601dd15dbb20ad3b9d222797
Pierre Jacquier <pierrejacquier39@gmail.com>
2019-10-30 08:53:53 -0400

    WIP fixing running errors

--- 1d-burgers2/main.py
+++ 1d-burgers2/main.py
@@ -13,6 +13,7 @@ from plots import plot_results
 
 sys.path.append(os.path.join(eqnPath, "utils"))
 from podnn import PodnnModel
+from metrics import error_podnn
 
 # HYPER PARAMETERS
 if len(sys.argv) > 1:
@@ -26,11 +27,13 @@ class Burgers2PodnnModel(PodnnModel):
     # def u_1(x, mu):
     #     return x / (1 + np.exp(1/(4*mu)*(x**2 - 1/4)))
 
-    def u(x, t, mu):
+    def u(self, X, t, mu):
+        x = X[0]
         t0 = np.exp(1 / (8*mu))
         return (x/t) / (1 + np.sqrt(t/t0)*np.exp(x**2/(4*mu*t)))
-    
-model = Burgers2PodnnModel(hp["n_v"], hp["n_x"], hp["n_t"]) 
+
+
+model = Burgers2PodnnModel(hp["n_v"], hp["n_x"], hp["n_t"], eqnPath)
 
 X_v_train, v_train, \
         X_v_val, v_val, U_val = model.generate_dataset(hp["x_min"], hp["x_max"],
@@ -40,7 +43,7 @@ X_v_train, v_train, \
                                                        hp["eps"])
 
 def error_val():
-    v_pred = regnn.predict(X_v_val)
+    v_pred = model.predict(X_v_val)
     return error_podnn(U_val, V.dot(v_pred.T))
 model.train(X_v_train, v_train, error_val, hp["h_layers"],
             hp["tf_epochs"], hp["learning_rate"], hp["lambda"]) 
--- utils/podnn.py
+++ utils/podnn.py
@@ -6,9 +6,12 @@ from tqdm import tqdm
 import pickle
 from pyDOE import lhs
 
+sys.path.append("utils")
+from pod import get_pod_bases
+
 
 class PodnnModel(object):
-    def __init__(self, n_v, n_x, n_t):
+    def __init__(self, n_v, n_x, n_t, eqnPath):
         # Dimension of the function output
         self.n_v = n_v
         # List of number of nodes in each direction of x
@@ -16,19 +19,23 @@ class PodnnModel(object):
         # Number of time steps
         self.n_t = n_t
 
-    def auto_meshgrid(x, t):
+        self.eqnPath = eqnPath
+
+    def auto_meshgrid(self, x, t):
         X0, *XT = np.meshgrid(*x, t)
-        X = XT[:-1]
+        X = [X0]
+        if len(XT) > 1: 
+            X.append(XT[:-1])
         T = XT[-1]
         return X, T
 
-    def u(x, t, mu):
+    def u(self, X, t, mu):
         return
 
-    def u_array(x, t, mu):
+    def u_array(self, x, t, mu):
         X, T = self.auto_meshgrid(x, t)
         U = np.zeros(self.get_n_x_tuple() + (self.n_t,))
-        U = u(*X, T, mu)
+        U = self.u(X, T, mu)
         return U
 
     def sample_mu(self, n_s, mu_min, mu_max):
@@ -38,19 +45,21 @@ class PodnnModel(object):
         mu_lhs = mu_min + (mu_max - mu_min)*X_lhs
         pbar.update(50)
         pbar.close()
-        return
+        return mu_lhs
 
     def get_n_x_tuple(self):
-        t = ()
+        tup = ()
         for n_x_i in self.n_x:
-            t += (n_x_i,)
-        return t
+            tup += (n_x_i,)
+        return tup
 
-    def create_snapshots(self, nn_s, n_d, n_h, x_min, x_max, t_min, t_max):
+    def create_snapshots(self, n_s, nn_s, n_d, n_h,
+                         x_min, x_max, t_min, t_max, mu_lhs):
         X_v = np.zeros((nn_s, n_d))
         U = np.zeros((n_h, nn_s))
-        U_struct = np.zeros(self.get_n_x_tuple() + (n_t, n_s))
-        x = [np.linspace(x_min[i], x_max[i], self.n_x[i]) for i in range(len(self.n_x))]
+        U_struct = np.zeros(self.get_n_x_tuple() + (self.n_t, n_s))
+        x = [np.linspace(x_min[i], x_max[i], self.n_x[i])
+             for i in range(len(self.n_x))]
         t = np.linspace(t_min, t_max, self.n_t)
         tT = t.reshape((self.n_t, 1))
         for i in tqdm(range(n_s)):
@@ -58,8 +67,10 @@ class PodnnModel(object):
             s = self.n_t * i
             e = self.n_t * (i + 1)
             X_v[s:e, :] = np.hstack((tT, np.ones_like(tT)*mu_lhs[i]))
-            U[:, s:e] = np.reshape(self.u_array(x, t, mu_lhs[i, :]), (n_h, self.n_t))
-            U_struct[:, :, i] = np.reshape(U[:, s:e], self.get_n_x_tuple() + (self.n_t,))
+            U[:, s:e] = np.reshape(self.u_array(x, t, mu_lhs[i, :]),
+                                   (n_h, self.n_t))
+            U_struct[:, :, i] = np.reshape(U[:, s:e],
+                                           self.get_n_x_tuple() + (self.n_t,))
 
     def split_dataset(self, X_v, v, train_val_ratio, nn_s):
         nn_s_train = int(train_val_ratio * nn_s)
@@ -72,10 +83,15 @@ class PodnnModel(object):
                          train_val_ratio, eps, eps_init=None,
                          use_cache=False, save_cache=False):
         
+        cache_path = os.path.join(self.eqnPath, "cache", "prep_data.pkl")
         if use_cache and os.path.exists(cache_path):
             with open(cache_path, "rb") as f:
                 print("Loaded cached data")
                 return pickle.load(f)
+
+        x_min, x_max = np.array(x_min), np.array(x_max)
+        t_min, t_max = np.array(t_min), np.array(x_max)
+        mu_min, mu_max = np.array(mu_min), np.array(mu_max)
 
         # Total number of snapshots
         nn_s = self.n_t * n_s 
@@ -84,14 +100,15 @@ class PodnnModel(object):
         # Number of DOFs
         n_h = self.n_v * np.prod(self.n_x)
 
-
         # LHS sampling (first uniform, then perturbated)
         print("Doing the LHS sampling on the non-spatial params...")
         mu_lhs = self.sample_mu(n_s, mu_min, mu_max)
 
         # Creating the snapshots
         print(f"Generating {nn_s} corresponding snapshots")
-        X_v, U, U_struct = self.create_snapshots()
+        X_v, U, U_struct = self.create_snapshots(n_s, nn_s, n_d, n_h,
+                                                 x_min, x_max, t_min, t_max,
+                                                 mu_lhs)
 
         # Getting the POD bases, with u_L(x, mu) = V.u_rb(x, mu) ~= u_h(x, mu)
         # u_rb are the reduced coefficients we're looking for
@@ -101,18 +118,18 @@ class PodnnModel(object):
             self.V = get_pod_bases(U, eps)
 
         # Projecting
-        v = (V.T.dot(U)).T
+        v = (self.V.T.dot(U)).T
        
         # Splitting the dataset (X_v, v)
         X_v_train, v_train, X_v_val, v_val = self.split_dataset(X_v, v, train_val_ratio, nn_s)
        
         # Creating the validation snapshots matrix
-        U_val = V.dot(v_val.T)
+        U_val = self.V.dot(v_val.T)
 
-        if save_cache:
-            with open(cache_path, "wb") as f:
-                pickle.dump((X_v_train, v_train, X_v_val, v_val,
-                             lb, ub, V, U_val), f)
+        # if save_cache:
+        #     with open(cache_path, "wb") as f:
+        #         pickle.dump((X_v_train, v_train, X_v_val, v_val,
+        #                      lb, ub, V, U_val), f)
 
         return X_v_train, v_train, X_v_val, v_val, U_val
 
@@ -124,7 +141,7 @@ class PodnnModel(object):
         # Creating the neural net model, and logger
         # In: (t, mu)
         # Out: u_rb = (u_rb_1, u_rb_2, ..., u_rb_L)
-        hp["layers"] = pack_layers(n_d, hp["h_layers"], n_L)
+        layers = pack_layers(n_d, hp["h_layers"], n_L)
         logger = Logger(hp)
         regnn = RegNN(hp, logger, ub, lb)
 
@@ -138,7 +155,7 @@ class PodnnModel(object):
         regnn.fit(X_v_train, v_train)
 
         # Saving
-        regnn.save_to(os.path.join(eqnPath, "cache", "model.h5"))
+        regnn.save_to(os.path.join(self.eqnPath, "cache", "model.h5"))
         
         return regnn
 
