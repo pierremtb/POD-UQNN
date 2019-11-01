57e9ea0c563cf9dd9adc24935f3c80dc4046cbf4
Pierre Jacquier <pierrejacquier39@gmail.com>
2019-11-01 08:42:37 -0400

    WIP cleaning up

--- 1d-burgers2/main.py
+++ 1d-burgers2/main.py
@@ -6,9 +6,7 @@ import tensorflow as tf
 
 eqnPath = "1d-burgers2"
 sys.path.append(eqnPath)
-from dataprep import prep_data
-from regression import create_model_and_train
-from predictions import predict_and_assess
+from datagen import u
 from plots import plot_results
 
 sys.path.append(os.path.join(eqnPath, "utils"))
@@ -17,47 +15,45 @@ from metrics import error_podnn
 from mesh import create_linear_mesh
 
 
-# HYPER PARAMETERS
-if len(sys.argv) > 1:
-    with open(sys.argv[1]) as hpFile:
-        hp = json.load(hpFile)
-else:
-    from hyperparams import hp
+if __name__ == "__main__":
 
+    # HYPER PARAMETERS
+    if len(sys.argv) > 1:
+        with open(sys.argv[1]) as hpFile:
+            hp = json.load(hpFile)
+    else:
+        from hyperparams import hp
 
-class Burgers2PodnnModel(PodnnModel):
-    def u_0(self, X, mu):
-        x = X[0]
-        return x / (1 + np.exp(1/(4*mu)*(x**2 - 1/4)))
+    # Create linear space mesh
+    x_mesh = create_linear_mesh(hp["x_min"], hp["x_max"], hp["n_x"])
 
-    def u(self, X, t, mu):
-        x = X[0]
-        t0 = np.exp(1 / (8*mu))
-        return (x/t) / (1 + np.sqrt(t/t0)*np.exp(x**2/(4*mu*t)))
+    # Extend the class and init the model
+    class Burgers2PodnnModel(PodnnModel):
+        def u(self, X, t, mu):
+            return u(X, t, mu)
+    model = Burgers2PodnnModel(hp["n_v"], x_mesh, hp["n_t"], eqnPath)
 
+    # Generate the dataset from the mesh and params
+    X_v_train, v_train, \
+        X_v_val, v_val, \
+        U_val = model.generate_dataset(hp["t_min"], hp["t_max"],
+                                       hp["mu_min"], hp["mu_max"],
+                                       hp["n_s"],
+                                       hp["train_val_ratio"],
+                                       hp["eps"])
 
-x_mesh = create_linear_mesh(hp["x_min"], hp["x_max"], hp["n_x"])
-model = Burgers2PodnnModel(hp["n_v"], x_mesh, hp["n_t"], eqnPath)
+    # Train
+    def error_val():
+        U_pred = model.predict(X_v_val)
+        return error_podnn(U_val, U_pred)
+    model.train(X_v_train, v_train, error_val, hp["h_layers"],
+                hp["epochs"], hp["lr"], hp["lambda"]) 
 
-X_v_train, v_train, \
-    X_v_val, v_val, \
-    U_val = model.generate_dataset(hp["t_min"], hp["t_max"],
-                                   hp["mu_min"], hp["mu_max"],
-                                   hp["n_s"],
-                                   hp["train_val_ratio"],
-                                   hp["eps"])
-
-def error_val():
+    # Predict and restruct
     U_pred = model.predict(X_v_val)
-    return error_podnn(U_val, U_pred)
-model.train(X_v_train, v_train, error_val, hp["h_layers"],
-            hp["epochs"], hp["lr"], hp["lambda"]) 
-
-U_pred = model.predict(X_v_val)
-
-U_pred_struct = model.restruct(U_pred)
-U_val_struct = model.restruct(U_val)
- 
-# PLOTTING AND SAVING RESULTS
-plot_results(U_val_struct, U_pred_struct, hp, eqnPath)
-plot_results(U_val_struct, U_pred_struct, hp)
+    U_pred_struct = model.restruct(U_pred)
+    U_val_struct = model.restruct(U_val)
+     
+    # PLOTTING AND SAVING RESULTS
+    plot_results(U_val_struct, U_pred_struct, hp, eqnPath)
+    plot_results(U_val_struct, U_pred_struct, hp)
