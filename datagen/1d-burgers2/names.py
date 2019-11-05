f688f9e478dfa69513c4265f20a38dabe68e21c9
Pierre Jacquier <pierrejacquier39@gmail.com>
2019-11-05 09:41:23 -0500

    Delete datagen/

--- datagen/1d-burgers2/names.py
+++
@@ -1,5 +0,0 @@
-X_FILE = "x.npy"
-T_FILE = "t.npy"
-U_MEAN_FILE = "u_mean.npy"
-U_STD_FILE = "u_std.npy"
-HP_FILE = "hp.json"
--- datagen/2d-ackley/names.py
+++
@@ -1,4 +0,0 @@
-X_FILE = "X.npy"
-Y_FILE = "Y.npy"
-U_MEAN_FILE = "u_mean.npy"
-U_STD_FILE = "u_std.npy"
