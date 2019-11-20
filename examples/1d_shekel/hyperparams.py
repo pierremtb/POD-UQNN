Fixed error that was outputing wrong results

examples/1d_shekel/hyperparams.py
@@ -32,7 +32,7 @@ HP["b1"] = 0.9
 HP["eps"] = 1e-10
 HP["lambda"] = 1e-6
 # Frequency of the logger
-HP["log_frequency"] = 1000
+HP["log_frequency"] = 2000
 # Non-spatial params
 bet = 1/10 * np.array([1, 2, 2, 4, 4, 6, 3, 7, 5, 5])
 gam = 1. * np.array([4, 1, 8, 6, 3, 2, 5, 8, 6, 7])

examples/2d_shallowwater/main.py
@@ -49,10 +49,11 @@ def main(hp, use_cached_dataset=False):
         U_val_pred_s = model.restruct(U_val_pred) 
         err = np.zeros((U_val_s.shape[0],))
         for i in range(err.shape[0]):
-            err[i] = error_podnn(U_val[i], U_val_pred[i])
+            err[i] = error_podnn(U_val_s[i], U_val_pred_s[i])
         return err
     model.train(X_v_train, v_train, error_val, hp["h_layers"],
-                hp["epochs"], hp["lr"], hp["lambda"], hp["decay"])
+                hp["epochs"], hp["lr"], hp["lambda"], hp["decay"],
+                hp["log_frequency"])
 
     # Predict and restruct
     U_pred = model.predict(X_v_val)