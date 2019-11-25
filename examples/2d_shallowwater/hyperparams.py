72b917d271f080d9afe210cec4964db0340a410e
Pierre Jacquier <pierrejacquier39@gmail.com>
2019-11-25 12:21:06 -0500

    Best parameters yet for 2d_s

--- examples/2d_shallowwater/hyperparams.py
+++ examples/2d_shallowwater/hyperparams.py
@@ -12,25 +12,20 @@ HP["n_v"] = len(HP["mesh_idx"][2])
 HP["n_t"] = 0
 # Snapshots count
 HP["n_s"] = 300
+# HP["n_s"] = 100
 # POD stopping param
 HP["eps"] = 1e-10
 # Train/val split
-HP["train_val_ratio"] = 0.7
+HP["train_val_ratio"] = 0.5
 # Deep NN hidden layers topology
 HP["h_layers"] = [64, 64]
 # Setting up the TF SGD-based optimizer
 HP["epochs"] = 200000
-# HP["epochs"] = 40000
-# HP["lr"] = 1e-1
-# HP["decay"] = 1e-2
-HP["lr"] = 0.001
-# HP["lr"] = 0.0005
+HP["lr"] = 0.005
 HP["decay"] = 0.
-# HP["lambda"] = 1e-4
-HP["lambda"] = 1e-4
-# HP["lambda"] = 0.
+HP["lambda"] = 1e-8
 # Frequency of the logger
-HP["log_frequency"] = 5000
+HP["log_frequency"] = 1000
 
 np.random.seed(1111)
 tf.random.set_seed(1111)
--- examples/2d_shallowwater/main.py
+++ examples/2d_shallowwater/main.py
@@ -78,5 +78,5 @@ if __name__ == "__main__":
     else:
         from hyperparams import HP
 
-    # main(HP, use_cached_dataset=False)
-    main(HP, use_cached_dataset=True)
+    main(HP, use_cached_dataset=False)
+    # main(HP, use_cached_dataset=True)
