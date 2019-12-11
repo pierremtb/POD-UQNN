8e3fa6526c14bd893e2e773d456957620ea4b574
Pierre Jacquier <pierrejacquier39@gmail.com>
2019-12-11 16:03:23 -0500

    Added ackley

--- examples/2d_ackley/main.py
+++ examples/2d_ackley/main.py
@@ -42,12 +42,24 @@ def main(hp, gen_test=False, use_cached_dataset=False,
                                         use_cache=use_cached_dataset)
 
     # Train
-    model.initNN(hp["h_layers"], hp["lr"], hp["lambda"])
+    model.initNN(hp["h_layers"], hp["h_layers_t"],
+                 hp["lr"], hp["lambda"], hp["beta"],
+                 hp["k1"], hp["k2"])
     train_res = model.train(X_v_train, v_train, hp["epochs"],
                             hp["train_val_test"], freq=hp["log_frequency"])
 
     # Predict and restruct
-    U_pred = model.predict(X_v_test)
+    v_pred, v_pred_sig = model.predict_v(X_v_test)
+    U_pred = model.V.dot(v_pred.T)
+    Sigma_pred = model.V.dot(v_pred_sig.T)
+    # U_pred_m = U_pred.mean(-1)
+    # U_test_m = U_test.mean(-1)
+    # Sigma_pred_m = Sigma_pred.mean(-1)
+    U_pred_m = U_pred[:, 50]
+    U_test_m = U_test.[:, 50]
+    Sigma_pred_m = Sigma_pred[:, 50]
+
+    # Predict and restruct
     U_pred = model.restruct(U_pred)
     U_test = model.restruct(U_test)
 
@@ -59,6 +71,8 @@ def main(hp, gen_test=False, use_cached_dataset=False,
     X_v_test_hifi = model.generate_hifi_inputs(hp["n_s_hifi"],
                                                hp["mu_min"], hp["mu_max"])
     U_pred_hifi_mean, U_pred_hifi_std = model.predict_heavy(X_v_test_hifi)
+    U_pred_hifi_mean = model.restruct(U_pred_hifi_mean[0], no_s=True), model.restruct(U_pred_hifi_mean[1], no_s=True)
+    U_pred_hifi_std = model.restruct(U_pred_hifi_std[0], no_s=True), model.restruct(U_pred_hifi_std[1], no_s=True)
 
     # Plot against test and save
     return plot_results(U_pred, U_pred_hifi_mean, U_pred_hifi_std,
