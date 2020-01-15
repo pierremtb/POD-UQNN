65f370eb24ce7f69282dd7b0e7cf5bf2c1376125
Pierre Jacquier <pierrejacquier39@gmail.com>
2020-01-15 09:13:17 -0500

    Started ensembles

--- examples/1d_cubic/main.py
+++ examples/1d_cubic/main.py
@@ -13,16 +13,18 @@ from podnn.podnnmodel import PodnnModel
 from podnn.mesh import create_linear_mesh
 from podnn.plotting import genresultdir
 
-from podnn.neuralnetwork import NeuralNetwork
+from podnn.varneuralnetwork import VarNeuralNetwork
 from podnn.metrics import re_mean_std, re
 from podnn.mesh import create_linear_mesh
 from podnn.logger import Logger
 from podnn.advneuralnetwork import NORM_MEANSTD, NORM_NONE
 
 
+# Datagen
 x_star = np.linspace(-6, 6, 100).reshape(100, 1)
 u_star = x_star**3
-
+x_val = np.linspace(-4, 4, 100).reshape(100, 1)
+u_val = x_star**3
 N = 20
 lb = int(2/(2*6) * 100)
 ub = int((2+2*4)/(2*6) * 100)
@@ -32,34 +34,48 @@ u_train = u_star[lb + idx]
 noise_std = 6
 u_train = u_train + noise_std*np.random.randn(u_train.shape[0], u_train.shape[1])
 
-layers = [1, 64, 64, 1]
-model = NeuralNetwork(layers, 0.001, 1e-10)
-model.summary()
+# Model creation
+def gen_and_train_model():
+    layers = [1, 50, 50, 1]
+    epochs = 5000
+    lr = 0.001
+    model = VarNeuralNetwork(layers, lr, 1e-10, lb=x_train.mean(), ub=x_train.std())
+    model.summary()
+    logger = Logger(epochs, 100)
 
-epochs = 30000
-logger = Logger(epochs, 1000)
+    def get_val_err():
+        u_val_pred, u_val_pred_var = model.predict(x_val)
+        return {
+            "RE": re(u_val_pred, u_val),
+            "var": u_val_pred_var.mean(),
+        }
+    logger.set_val_err_fn(get_val_err)
 
-def get_val_err():
-    u_pred = model.predict(x_star)
-    return {
-        "RE": re(u_pred, u_star),
-    }
-logger.set_val_err_fn(get_val_err)
+    # Training
+    model.fit(x_train, u_train, epochs, logger)
 
-# Training
-model.fit(x_train, u_train, epochs, logger)
+    # Make the prediction on the meshed x-axis (ask for MSE as well)
+    return model.predict(x_star)
 
-# Make the prediction on the meshed x-axis (ask for MSE as well)
-u_pred = model.predict(x_star)
+M = 5
+u_pred_samples = np.zeros((100, 1, M))
+u_pred_var_samples = np.zeros((100, 1, M))
+for i in range(0, M):
+    u_pred_samples[:, :, i], u_pred_var_samples[:, :, i] = gen_and_train_model()
+
+u_pred = u_pred_samples.mean(-1)
+u_pred_var = (u_pred_var_samples + u_pred_samples ** 2).mean(-1) - u_pred ** 2
+
+
 # u_pred, u_pred_var = model.predict_f(x_star)
 # u_pred_samples = model.predict_f_samples(x_star, 10)
 
 plt.plot(x_star, u_star)
 plt.scatter(x_train, u_train)
 plt.plot(x_star, u_pred, "r--")
-# lower = u_pred - 2 * np.sqrt(u_pred_var)
-# upper = u_pred + 2 * np.sqrt(u_pred_var)
-# plt.fill_between(x_star[:, 0], lower[:, 0], upper[:, 0], 
-#                     facecolor='orange', alpha=0.5, label=r"$2\sigma_{T,hf}(x)$")
+lower = u_pred - 2 * np.sqrt(u_pred_var)
+upper = u_pred + 2 * np.sqrt(u_pred_var)
+plt.fill_between(x_star[:, 0], lower[:, 0], upper[:, 0], 
+                    facecolor='orange', alpha=0.5, label=r"$2\sigma_{T,hf}(x)$")
 # plt.plot(x_star, u_pred_samples[:, :, 0].numpy().T, 'orange', linewidth=.5)
 plt.show()
---
+++ podnn/varneuralnetwork.py
@@ -0,0 +1,172 @@
+
+"""Module with a class defining an Artificial Neural Network."""
+
+import os
+import pickle
+import tensorflow as tf
+from sklearn.preprocessing import normalize as sknormalize
+import numpy as np
+from tqdm import tqdm
+
+
+class VarNeuralNetwork:
+    def __init__(self, layers, lr, lam, model=None, lb=None, ub=None):
+        # Making sure the dtype is consistent
+        self.dtype = "float32"
+
+        # Setting up optimizer and params
+        self.tf_optimizer = tf.keras.optimizers.Adam(lr)
+        self.layers = layers
+        self.lr = lr
+        self.lam = lam
+        self.lb = lb
+        self.ub = ub
+        self.logger = None
+        self.batch_size = 0
+
+        # Setting up the model
+        tf.keras.backend.set_floatx(self.dtype)
+        if model is None:
+            self.model = self.build_model()
+        else:
+            self.model = model
+
+    def build_model(self):
+        """Descriptive Keras model."""
+
+        inputs = tf.keras.Input(shape=(self.layers[0],), name="x", dtype=self.dtype)
+
+        x = inputs
+        for width in self.layers[1:-1]:
+            x = tf.keras.layers.Dense(
+                    width, activation=tf.nn.relu, dtype=self.dtype,
+                    kernel_initializer="glorot_normal")(x)
+        x = tf.keras.layers.Dense(
+                2 * self.layers[-1], activation=None, dtype=self.dtype,
+                kernel_initializer="glorot_normal")(x)
+
+        # Output processing function
+        def split_mean_var(data):
+            mean, out_var = tf.split(data, num_or_size_splits=2, axis=1)
+            var = tf.math.log(1.0 + tf.exp(out_var)) + 1e-4
+            return [mean, var]
+        
+        outputs = tf.keras.layers.Lambda(split_mean_var)(x)
+
+        model = tf.keras.Model(inputs=inputs, outputs=outputs, name="varnn")
+
+        return model
+
+    def normalize(self, X):
+        """Apply a kind of normalization to the inputs X."""
+        if self.lb is not None and self.ub is not None:
+            # X = (X - self.lb) - 0.5*(self.ub - self.lb)
+            # X = sknormalize(X, norm="max")
+            # X = (X - X.mean(0)) / X.std(0)
+            X = (X - self.lb) / self.ub
+        return self.tensor(X)
+
+    def regularization(self):
+        l2_norms = [tf.nn.l2_loss(v) for v in self.wrap_training_variables()]
+        l2_norm = tf.reduce_sum(l2_norms)
+        return self.lam * l2_norm
+
+    # @tf.function
+    def loss(self, y, y_pred):
+        """Return the Gaussian NLL loss function between the pred and val."""
+        y_pred_mean, y_pred_var = y_pred
+        return tf.reduce_mean(tf.math.log(y_pred_var + 1e-4)) / 2 + \
+               tf.reduce_mean(tf.divide(tf.square(y -  y_pred_mean), y_pred_var + 1e-4)) / 2 + \
+               tf.math.log(2*np.pi) / 2
+
+    # @tf.function
+    def grad(self, X, v):
+        """Compute the loss and its derivatives w.r.t. the inputs."""
+        with tf.GradientTape() as tape:
+            loss_value = self.loss(v, self.model(X))
+        grads = tape.gradient(loss_value, self.wrap_training_variables())
+        return loss_value, grads
+
+    def wrap_training_variables(self):
+        """Convenience method. Should be extended if needed."""
+        var = self.model.trainable_variables
+        return var
+
+    def tf_optimization(self, X_v, v, tf_epochs):
+        """Run the training loop."""
+        for epoch in range(tf_epochs):
+            loss_value = self.tf_optimization_step(X_v, v)
+            self.logger.log_train_epoch(epoch, loss_value)
+        return loss_value
+
+    @tf.function
+    def tf_optimization_step(self, X_v, v):
+        """For each epoch, get loss+grad and backpropagate it."""
+        loss_value, grads = self.grad(X_v, v)
+        self.tf_optimizer.apply_gradients(
+            zip(grads, self.wrap_training_variables()))
+        return loss_value
+
+    def fit(self, X_v, v, epochs, logger):
+        """Train the model over a given dataset, and parameters."""
+        # Setting up logger
+        self.logger = logger
+        self.logger.log_train_start()
+
+        # Normalizing and preparing inputs
+        # X_v = self.normalize(X_v)
+        X_v = self.tensor(X_v)
+        v = self.tensor(v)
+        print(X_v.shape)
+        print(v.shape)
+
+        # Optimizing
+        last_loss = self.tf_optimization(X_v, v, epochs)
+
+        self.logger.log_train_end(epochs, last_loss)
+
+    def fetch_minibatch(self, X_v, v):
+        """Return a subset of the training set, for lower memory training."""
+        if self.batch_size < 1:
+            return self.tensor(X_v), self.tensor(v)
+        N_v = X_v.shape[0]
+        idx_v = np.random.choice(N_v, self.batch_size, replace=False)
+        X_v_batch = self.tensor(X_v[idx_v, :])
+        v_batch = self.tensor(v[idx_v, :])
+        return X_v_batch, v_batch
+
+    def predict(self, X):
+        """Get the prediction for a new input X."""
+        X = self.normalize(X)
+        y_pred_mean, y_pred_var = self.model(X)
+        return y_pred_mean.numpy(), y_pred_var.numpy()
+
+    def summary(self):
+        """Print a summary of the TensorFlow/Keras model."""
+        return self.model.summary()
+
+    def tensor(self, X):
+        """Convert input into a TensorFlow Tensor with the class dtype."""
+        return tf.convert_to_tensor(X, dtype=self.dtype)
+
+    def save_to(self, model_path, params_path):
+        """Save the (trained) model and params for later use."""
+        with open(params_path, "wb") as f:
+            pickle.dump((self.layers, self.lr, self.lam, self.lb, self.ub), f)
+        tf.keras.models.save_model(self.model, model_path)
+
+    @classmethod
+    def load_from(cls, model_path, params_path):
+        """Load a (trained) model and params."""
+
+        if not os.path.exists(model_path):
+            raise FileNotFoundError("Can't find cached model.")
+        if not os.path.exists(params_path):
+            raise FileNotFoundError("Can't find cached model params.")
+
+        print(f"Loading model from {model_path}")
+        with open(params_path, "rb") as f:
+            layers, lam, lr, lb, ub = pickle.load(f)
+        print(f"Loading model params from {params_path}")
+        model = tf.keras.models.load_model(model_path)
+        return cls(layers, lam, lr, model=model, lb=lb, ub=ub)
