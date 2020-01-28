d9ff3ed79433060a801a1bb528329d3270da064f
Pierre Jacquier <pierrejacquier39@gmail.com>
2020-01-28 09:28:03 -0500

    WIP

--- examples/1d_cubic/tfp-nonlinear.py
+++ examples/1d_cubic/tfp-nonlinear.py
@@ -7,7 +7,6 @@ import yaml
 import numpy as np
 import tensorflow as tf
 import tensorflow_probability as tfp
-import tensorflow_addons as tfa
 tfd = tfp.distributions
 tfk = tf.keras
 
@@ -46,22 +45,31 @@ def plot(x, y, x_tst, y_tst, yhat=None, yhats=[], lower=None, upper=None):
     plt.xlabel("$x$")
     plt.show()
 
+def normalize(x, mean, std):
+    return (x - mean) / std
+
 #%% Datagen
-N_star = 100
+N_star = 300
 D = 1
 x_tst = np.linspace(-6, 6, N_star).reshape((N_star, 1))
 y_tst = x_tst**3
 
 #%% Training split
-N = 20
+N = 150
 lb = int(2/(2*6) * N_star)
 ub = int((2+2*4)/(2*6) * N_star)
-# idx = np.random.choice(x_tst[lb:ub].shape[0], N, replace=False)
-idx = np.array([26, 23,  4,  3, 27, 64, 58, 30, 18, 16,  2, 31, 65, 15, 11, 17, 57, 28, 34, 50])
+idx = np.random.choice(x_tst[lb:ub].shape[0], N, replace=False)
+# idx = np.array([26, 23,  4,  3, 27, 64, 58, 30, 18, 16,  2, 31, 65, 15, 11, 17, 57, 28, 34, 50])
 x = x_tst[lb + idx]
+x_range = [x.min(), x.max()]
 y = y_tst[lb + idx]
 noise_std = 3
 y = y + noise_std*np.random.randn(y.shape[0], y.shape[1])
+
+mean = x.mean()
+std =  x.std()
+x_n = normalize(x, mean, std)
+x_tst_n = normalize(x_tst, mean, std)
 
 #%% Loss
 negloglik = lambda y, rv_y: -rv_y.log_prob(y)
@@ -77,10 +85,10 @@ model = tfk.Sequential([
 
 # Do inference.
 model.compile(optimizer=tf.optimizers.Adam(learning_rate=0.05), loss=negloglik)
-model.fit(x, y, epochs=500, verbose=False)
+model.fit(x_n, y, epochs=500, verbose=False)
 
 # Make predictions.
-yhat = model(x_tst)
+yhat = model(x_tst_n)
 plot(x, y, x_tst, y_tst, yhat.mean())
 
 #%% Case 2: known unknowns (aleatoric uncertainty)
@@ -97,10 +105,10 @@ model = tfk.Sequential([
 
 # Do inference.
 model.compile(optimizer=tf.optimizers.Adam(learning_rate=0.05), loss=negloglik)
-model.fit(x, y, epochs=1500, verbose=False)
+model.fit(x_n, y, epochs=1500, verbose=False)
 
 # Make predictions.
-yhat = model(x_tst)
+yhat = model(x_tst_n)
 lower = yhat.mean() - 3 * yhat.stddev()
 upper = yhat.mean() + 3 * yhat.stddev()
 plot(x, y, x_tst, y_tst, yhat.mean(), lower=lower, upper=upper)
@@ -141,10 +149,10 @@ model = tfk.Sequential([
 
 # Do inference.
 model.compile(optimizer=tf.optimizers.Adam(learning_rate=0.05), loss=negloglik)
-model.fit(x, y, batch_size=None, epochs=1500, verbose=False)
+model.fit(x_n, y, batch_size=None, epochs=1500, verbose=False)
 
 # Make predictions.
-yhats = np.array([model(x_tst).mean() for _ in range(100)])
+yhats = np.array([model(x_tst_n).mean() for _ in range(100)])
 plot(x, y, x_tst, y_tst, yhats.mean(0), yhats=yhats)
 
 #%% Case 4: both unknowns
@@ -165,10 +173,10 @@ model = tf.keras.Sequential([
 
 # Do inference.
 model.compile(optimizer=tf.optimizers.Adam(learning_rate=0.005), loss=negloglik)
-model.fit(x, y, epochs=10000, verbose=False)
+model.fit(x_n, y, epochs=10000, verbose=False)
 
 # Profit.
-yhats = [model(x_tst) for _ in range(100)]
+yhats = [model(x_tst_n) for _ in range(100)]
 yhats_mean = np.array([yh.mean() for yh in yhats])
 yhats_var = np.array([yh.variance() for yh in yhats])
 yhat = yhats_mean.mean(0)
@@ -176,3 +184,71 @@ yhat_var = (yhats_var + yhat ** 2).mean(0) - yhat ** 2
 lower = yhat - 3 * np.sqrt(yhat_var)
 upper = yhat + 3 * np.sqrt(yhat_var)
 plot(x, y, x_tst, y_tst, yhat, lower=lower, upper=upper)
+
+#%% Additionnal: GP
+
+class RBFKernelFn(tf.keras.layers.Layer):
+  def __init__(self, **kwargs):
+    super(RBFKernelFn, self).__init__(**kwargs)
+    dtype = kwargs.get('dtype', None)
+
+    self._amplitude = self.add_variable(
+            initializer=tf.constant_initializer(0),
+            dtype=dtype,
+            name='amplitude')
+    
+    self._length_scale = self.add_variable(
+            initializer=tf.constant_initializer(0),
+            dtype=dtype,
+            name='length_scale')
+
+  def call(self, x):
+    # Never called -- this is just a layer so it can hold variables
+    # in a way Keras understands.
+    return x
+
+  @property
+  def kernel(self):
+    return tfp.math.psd_kernels.ExponentiatedQuadratic(
+      amplitude=tf.nn.softplus(0.1 * self._amplitude),
+      length_scale=tf.nn.softplus(5. * self._length_scale)
+    )
+# For numeric stability, set the default floating-point dtype to float64
+tf.keras.backend.set_floatx('float64')
+
+# Build model.
+num_inducing_points = 40
+model = tf.keras.Sequential([
+    tf.keras.layers.InputLayer(input_shape=[1]),
+    tf.keras.layers.Dense(1, kernel_initializer='ones', use_bias=False),
+    tfp.layers.VariationalGaussianProcess(
+        num_inducing_points=num_inducing_points,
+        kernel_provider=RBFKernelFn(),
+        event_shape=[1],
+        inducing_index_points_initializer=tf.constant_initializer(
+            np.linspace(*x_range, num=num_inducing_points,
+                        dtype=x.dtype)[..., np.newaxis]),
+        unconstrained_observation_noise_variance_initializer=(
+            tf.constant_initializer(np.array(0.54).astype(x.dtype))),
+    ),
+])
+
+# Do inference.
+batch_size = 32
+loss = lambda y, rv_y: rv_y.variational_loss(
+    y, kl_weight=np.array(batch_size, x.dtype) / x.shape[0])
+model.compile(optimizer=tf.optimizers.Adam(learning_rate=0.01), loss=loss)
+model.fit(x_n, y, batch_size=batch_size, epochs=1000, verbose=False)
+
+# Profit.
+yhat = model(x_tst_n)
+assert isinstance(yhat, tfd.Distribution)
+yhats = np.array([yhat.sample().numpy() for _ in range(100)])
+yhat_mean = yhats.mean(0)
+yhat_var = yhats.var(0)
+lower = yhat_mean - 3 * np.sqrt(yhat_var)
+upper = yhat_mean + 3 * np.sqrt(yhat_var)
+plot(x, y, x_tst, y_tst, yhat_mean, lower=lower, upper=upper, yhats=yhats[:5])
+
+
+# %%
