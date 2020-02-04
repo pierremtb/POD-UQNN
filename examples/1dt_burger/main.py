Fixed variance, added sample plotting on shekel

examples/1dt_burger/main.py
@@ -54,14 +54,15 @@ print("Sampling {n_s_hifi} parameters")
 X_v_test = model.generate_hifi_inputs(hp["n_s_hifi"],
                                       hp["mu_min"], hp["mu_max"], hp["t_min"], hp["t_max"])
 print("Predicting the {n_s_hifi} corresponding solutions")
-U_pred, U_pred_sig = model.predict(X_v_test)
-U_pred_mean = (model.restruct(U_pred.mean(-1), no_s=True),
-                    model.restruct(U_pred_sig.mean(-1), no_s=True))
-U_pred_var = ((U_pred_sig - model.pod_sig[:, np.newaxis])**2 + U_pred ** 2).mean(-1) - U_pred.mean(-1) ** 2
-U_pred_std = (model.restruct(U_pred.std(-1), no_s=True),
-                    model.restruct(np.sqrt(U_pred_var), no_s=True))
+U_pred_hifi, U_pred_hifi_sig = model.predict(X_v_test)
+U_pred_hifi = model.restruct(U_pred_hifi)
+U_pred_hifi_sig = model.restruct(U_pred_hifi_sig)
+U_pred_hifi_mean = (model.restruct(U_pred_hifi.mean(-1), no_s=True),
+                    model.restruct(U_pred_hifi_sig.mean(-1), no_s=True))
+U_pred_hifi_std = (model.restruct(U_pred_hifi.std(-1), no_s=True),
+                    model.restruct(U_pred_hifi_sig.std(-1), no_s=True))
 sigma_pod = model.pod_sig.mean()
 
 # Plot against test and save
-plot_results(U_val, U_val_pred, U_pred_mean, U_pred_std, sigma_pod,
+plot_results(U_val, U_val_pred, U_pred_hifi_mean, U_pred_hifi_std, sigma_pod,
              resdir, train_res[0], hp)

podnn/podnnmodel.py
@@ -390,21 +390,24 @@ class PodnnModel:
         return v_pred.astype(self.dtype), v_pred_sig.astype(self.dtype)
 
     def predict(self, X_v):
-        """Returns the predicted solutions, via proj coefficients."""
-        v_pred, v_pred_sig = self.predict_v(X_v)
-        v_pred_up = v_pred + v_pred_sig
+        """Return the predicted POD projection coefficients."""
+        n_M = len(self.regnn)
+        U_pred_samples = np.zeros((self.n_h, X_v.shape[0], n_M))
+        U_pred_sig_samples = np.zeros((self.n_h, X_v.shape[0], n_M))
 
-        # Retrieving the function with the predicted coefficients
-        U_pred = self.project_to_U(v_pred)
-        U_pred_up = self.project_to_U(v_pred_up)
+        for i, model in enumerate(self.regnn):
+            v_pred, v_pred_var = model.predict(X_v)
+            U_pred_samples[:, :, i] = self.project_to_U(v_pred)
+            U_pred_sig_samples[:, :, i] = self.project_to_U(np.sqrt(v_pred_var))
 
-        # Retrieving sig
-        U_pred_sig = np.abs(U_pred_up - U_pred)
+        U_pred = U_pred_samples.mean(-1)
+        U_pred_var = (U_pred_sig_samples**2 + U_pred_samples ** 2).mean(-1) - U_pred ** 2
+        U_pred_sig = np.sqrt(U_pred_var)
 
         if self.pod_sig is not None:
             U_pred_sig += self.pod_sig[:, np.newaxis]
 
-        return U_pred, U_pred_sig
+        return U_pred.astype(self.dtype), U_pred_sig.astype(self.dtype)
 
     def project_to_U(self, v):
         return self.V.dot(v.T)