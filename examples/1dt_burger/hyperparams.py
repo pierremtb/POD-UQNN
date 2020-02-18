6394eb645c0431420c9c3fc6832cb81d2764ec80
Pierre Jacquier <pierrejacquier39@gmail.com>
2020-02-18 12:36:07 -0500

    Ported burger

--- examples/1dt_burger/hyperparams.py
+++ examples/1dt_burger/hyperparams.py
@@ -1,5 +1,6 @@
 """Default hyperparameters for 1D time-dep Burgers Equation."""
 
+import numpy as np
 from podnn.advneuralnetwork import NORM_MEANSTD, NORM_CENTER, NORM_NONE
 
 HP = {}
@@ -39,3 +40,15 @@ HP["mu_min"] = [0.001]
 HP["mu_max"] = [0.0100]
 HP["mu_min_out"] = [0.0005]
 HP["mu_max_out"] = [0.0105]
+
+def u(X, t, mu):
+    """Burgers2 explicit solution."""
+    x = X[0]
+    mu = mu[0]
+
+    if t == 1.:
+        res = x / (1 + np.exp(1/(4*mu)*(x**2 - 1/4)))
+    else:
+        t0 = np.exp(1 / (8*mu))
+        res = (x/t) / (1 + np.sqrt(t/t0)*np.exp(x**2/(4*mu*t)))
+    return res.reshape((1, x.shape[0]))
