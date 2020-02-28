7ac4e11f0448799cb0a3d7007c30e361dc3eaf6e
Pierre Jacquier <pierrejacquier39@gmail.com>
2020-02-28 08:21:05 -0500

    New hps for 2dt

--- examples/2d_shallowwater/runner.sh
+++ examples/2d_shallowwater/runner.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --account=def-soulaima
-#SBATCH --gres=gpu:k80:5        # request GPU "generic resource"
+#SBATCH --gres=gpu:k80:1        # request GPU "generic resource"
 #SBATCH --cpus-per-task=6   # maximum CPU cores per GPU request: 6 on Cedar, 16 on Graham.
 #SBATCH --nodes=1        # nodes
-#SBATCH --mem=64000M        # memory per node
+#SBATCH --mem=128000M        # memory per node
 #SBATCH --time=0-02:00      # time (DD-HH:MM)
 #SBATCH --output=log-%j-%N.txt  # %N for node name, %j for jobID
 
@@ -12,13 +12,7 @@ source ~/env/bin/activate
 
 function run_ex() {
         cd $1
-        python gen.py
-        time horovodrun -np 5 -H localhost:5 python train.py --distribute
-        python pred.py
-        cd ..
+        python pred_ext.py
 }
 
-# run_ex 1d_shekel
-run_ex 2d_ackley
-# run_ex 1dt_burger
-# run_ex 2d_shallowwater
+run_ex 2d_shallowwater
--- examples/2dt_shallowwater/hyperparams.py
+++ examples/2dt_shallowwater/hyperparams.py
@@ -15,24 +15,24 @@ HP["n_v"] = len(HP["mesh_idx"][2])
 HP["n_t"] = 10
 HP["d_t"] = 1
 # Snapshots count
-HP["n_s"] = 3
+HP["n_s"] = 20
 HP["n_s_tst"] = 3
 # POD stopping param
 HP["eps"] = 0
 HP["n_L"] = 10
 # Train/val split
-HP["train_val"] = (3/5, 1/5)
+HP["train_val"] = (.8, .2)
 # Deep NN hidden layers topology
 HP["h_layers"] = [128, 128, 128]
 # Setting up TF SGD-based optimizer
 HP["n_M"] = 5
-HP["epochs"] = 10000
+HP["epochs"] = 40000
 HP["lr"] = 0.01
 HP["lambda"] = 0.001
 HP["adv_eps"] = 0.
 HP["norm"] = "none"
 # Frequency of the logger
-HP["log_frequency"] = 5000
+HP["log_frequency"] = 1000
 
 # np.random.seed(1111)
 # tf.random.set_seed(1111)
