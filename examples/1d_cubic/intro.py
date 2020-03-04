6dc004085d1111ebce119f19059d7656d8aacba8
Pierre Jacquier <pierrejacquier39@gmail.com>
2020-03-04 11:13:03 -0500

    Added gp graphs in intro

--- examples/1d_cubic/intro.py
+++ examples/1d_cubic/intro.py
@@ -149,7 +149,7 @@ y = y_tst[lb + idx]
 noise_std = 9
 y = y + noise_std*np.random.randn(y.shape[0], y.shape[1])
 #%%
-d = 10
+d = 3
 beta = 1/noise_std**2
 alpha = 5e-3
 
@@ -174,21 +174,21 @@ def variance(x_):
     return 1/beta + phi(x_).T.dot(S).dot(phi(x_))
 
 # Predictions
-y_pred = np.zeros_like(x_tst)
+f_post = np.zeros_like(x_tst)
 var = np.zeros_like(x_tst)
 for i, x_i in enumerate(x_tst):
-    y_pred[i] = mean(x_i)
+    f_post[i] = mean(x_i)
     var[i] = variance(x_i)
 sig = np.sqrt(var)
 
-upper = y_pred + 2 * sig
-lower = y_pred - 2 * sig
+upper = f_post + 2 * sig
+lower = f_post - 2 * sig
 
 fig = plt.figure(figsize=figsize(1, 1, scale=2.5))
 plt.fill_between(x_tst.ravel(), upper.ravel(), lower.ravel(), 
                     facecolor='C0', alpha=0.3, label=r"$2\sigma_{T}(x)$")
 plt.scatter(x, y, c="r", label=r"$y_i$")
-plt.plot(x_tst, y_pred, "b-", label=r"$y_i$")
+plt.plot(x_tst, f_post, "b-", label=r"$y_i$")
 plt.plot(x_tst, y_tst, "r--", label=r"$u(x)$")
 plt.legend()
 plt.ylim((y_tst.min(), y_tst.max()))
@@ -201,152 +201,209 @@ tfk = tf.keras
 model = tfk.Sequential([
     tfk.layers.Dense(5,
         activation="relu",
-        kernel_regularizer=tfk.regularizers.l2(0.01)),
+        kernel_regularizer=tfk.regularizers.l2(0.001)),
     tfk.layers.Dense(1),
 ])
 model.compile(optimizer=tfk.optimizers.Adam(0.1), loss="mse")
 model.fit(x, y, epochs=1000, verbose=0)
-y_pred = model.predict(x_tst)
+f_post = model.predict(x_tst)
 
 fig = plt.figure(figsize=figsize(1, 1, scale=2.5))
 plt.scatter(x, y, c="r", label=r"$y_i$")
-plt.plot(x_tst, y_pred, "b-", label=r"$y_i$")
+plt.plot(x_tst, f_post, "b-", label=r"$y_i$")
 plt.plot(x_tst, y_tst, "r--", label=r"$u(x)$")
 plt.legend()
 plt.ylim((y_tst.min(), y_tst.max()))
 plt.savefig(f"review-bishop-toy-nn.pdf", bbox_inches='tight', pad_inches=0)
 
-
-
-
-
-
-
-
-
-
-
-
-
 #%%
-import sys
-import os
-import tensorflow as tf
-import tensorflow_probability as tfp
 
-tfd = tfp.distributions
-tfk = tf.keras
-dtype = "float64"
-tf.keras.backend.set_floatx(dtype)
+# Datagen
+N_tst = 50
+x_tst = np.linspace(-5, 5, N_tst).reshape(-1,1)
+y_tst = np.sin(x_tst)
+x = np.array([-4, -3, -2, -1, 1]).reshape(5,1)
+y = np.sin(x)
 
-sys.path.append(os.path.join("..", ".."))
-from podnn.plotting import figsize
-#%% Model creation
-def prior_trainable(kernel_size, bias_size=0, dtype=None):
-    n = kernel_size + bias_size
-    return tfk.models.Sequential([
-        # tfp.layers.VariableLayer(n, dtype=dtype, trainable=False),
-        tfp.layers.VariableLayer(n, dtype=dtype, trainable=True),
-        tfp.layers.DistributionLambda(lambda t: tfd.Independent(
-            tfd.Normal(loc=t, scale=1),
-            reinterpreted_batch_ndims=1,
-        ))
-    ])
+# Kernel and covariance
+def kernel(a, b, param):
+    sqdist = np.sum(a**2,1).reshape(-1,1) + np.sum(b**2,1) - 2*np.dot(a, b.T)
+    return np.exp(-.5 * (1/param) * sqdist)
+param = 0.1
+K_ss = kernel(x_tst, x_tst, param)
 
-def posterior_mean_field(kernel_size, bias_size=0, dtype=None):
-    n = kernel_size + bias_size
-    c = tf.math.log(tf.math.expm1(tf.constant(1., dtype=dtype)))
-    return tfk.models.Sequential([
-        tfp.layers.VariableLayer(2 * n, dtype=dtype),
-        tfp.layers.DistributionLambda(lambda t: tfd.Independent(
-            tfd.Normal(
-                loc=t[..., :n],
-                scale=1e-5 + 1. * tf.math.softplus(c + t[..., n:]),
-            ),
-            reinterpreted_batch_ndims=1,
-        ))
-    ])
+# Get cholesky decomposition of covariance matrix
+L = np.linalg.cholesky(K_ss + 1e-15*np.eye(N_tst))
 
-model = tfk.models.Sequential([
-    # tfk.layers.Dense(8, activation="linear"),
-    # tfk.layers.Dense(2, activation="linear"),
-    tfp.layers.DenseVariational(
-        units=8,
-        activation="relu",
-        make_posterior_fn=posterior_mean_field,
-        make_prior_fn=prior_trainable,
-        kl_weight=1/N,
-        dtype=dtype,
-    ),
-    tfp.layers.DenseVariational(
-        units=2,
-        activation="linear",
-        make_posterior_fn=posterior_mean_field,
-        make_prior_fn=prior_trainable,
-        kl_weight=1/N,
-        dtype=dtype,
-    ),
-    tfp.layers.DistributionLambda(lambda t:
-        tfd.Normal(
-            loc=t[..., :1],
-            scale=1e-3 + tf.math.softplus(0.01 * t[..., 1:]),
-        ),
-        # tfd.Normal(
-        #     loc=t,
-        #     scale=1,
-        # ),
-    ),
-])
-lr = 0.01
-model.compile(loss=lambda y, model: -model.log_prob(y), optimizer=tfk.optimizers.Adam(lr))
-epochs = 12000
-model.fit(x, y, epochs=epochs, verbose=0)
+# Sample 3 sets of standard normals for our test points,
+# multiply them by the square root of the covariance matrix
+f_prior = np.dot(L, np.random.normal(size=(N_tst,3)))
 
-# yhat = model(x_tst)
-# yhats = [model(x_tst) for _ in range(100)]
-##%% Predictions and plotting
+# Apply the kernel function to our training points
+K = kernel(x, x, param)
+L = np.linalg.cholesky(K + 0.00005*np.eye(len(x)))
 
-y_pred_list = []
-y_pred_var_list = []
-for i in range(200):
-    yhat = model(x_tst)
-    y_pred_list.append(yhat.mean().numpy())
-    y_pred_var_list.append(yhat.variance().numpy())
+# Compute the mean at our test points and standard deviation.
+K_s = kernel(x, x_tst, param)
+Lk = np.linalg.solve(L, K_s)
+mean = np.dot(Lk.T, np.linalg.solve(L, y)).reshape((N_tst,))
+var = np.diag(K_ss) - np.sum(Lk**2, axis=0)
+sig = np.sqrt(var)
 
-u_pred = np.array(y_pred_list).mean(0)
-u_pred_var = (np.array(y_pred_list)**2 + np.array(y_pred_var_list)).mean(0) - np.array(y_pred_list).mean(0) ** 2
-u_pred_sig = np.sqrt(u_pred_var)
+# Draw 3 samples from the posterior
+L = np.linalg.cholesky(K_ss + 1e-6*np.eye(N_tst) - np.dot(Lk.T, Lk))
+f_post = mean.reshape(-1,1) + np.dot(L, np.random.normal(size=(N_tst,3)))
 
-# y_preds = np.concatenate(y_pred_list, axis=1)
-# u_pred = np.mean(y_preds, axis=1)
-# u_pred_sig = np.std(y_preds, axis=1)
-
-# u_pred = yhat.mean().numpy()
-# u_pred_sig = yhat.stddev().numpy()
-lower = u_pred - 2 * u_pred_sig
-upper = u_pred + 2 * u_pred_sig
-
-fig = plt.figure(figsize=figsize(1, 1, scale=2.5))
+lower = mean - 2 * sig
+upper = mean + 2 * sig
+fig = plt.figure(figsize=figsize(2, 2, scale=2.5))
+gs = fig.add_gridspec(2, 2)
+ax = fig.add_subplot(gs[0, :])
+plt.axis([-5, 5, -3, 3])
+ax.plot(x_tst, f_prior)
+ax.set_title("Prior samples")
+ax.set_xlabel("$x$")
+ax = fig.add_subplot(gs[1, :])
 plt.fill_between(x_tst.ravel(), upper.ravel(), lower.ravel(), 
-                    facecolor='C0', alpha=0.3, label=r"$3\sigma_{T}(x)$")
-# plt.plot(x_star, u_pred_samples[:, :, 0].numpy().T, 'C0', linewidth=.5)
-plt.plot(x_tst, u_pred, label=r"$\hat{u}_*(x)$")
-plt.scatter(x, y, c="r", label=r"$u_T(x)$")
-plt.plot(x_tst, y_tst, "r--", label=r"$u_*(x)$")
-# for yhat in yhats:
-#     plt.plot(x_tst, yhat.mean().numpy(), "b-", alpha=0.5)
-#     plt.plot(x_tst, yhat.mean().numpy() + 2*yhat.stddev().numpy(), "b-", alpha=0.01)
-#     plt.plot(x_tst, yhat.mean().numpy() - 2*yhat.stddev().numpy(), "b-", alpha=0.01)
-plt.legend()
-plt.xlabel("$x$")
-plt.show()
-# exit(0)
-# # plt.savefig("results/gp.pdf")
-# plt.savefig("results/cos.pdf")
+                    facecolor='C0', alpha=0.2, label=r"$2\sigma_{T}(x)$")
+ax.scatter(x, y, c="r", label=r"$y_i$")
+ax.plot(x_tst, f_post, "b-", alpha=0.3)
+ax.plot(x_tst, mean, "b-", label=r"$\hat{u}(x)$")
+ax.plot(x_tst, y_tst, "r--", label=r"$u(x)$")
+ax.set_title("Observed data and posterior samples")
+ax.set_xlabel("$x$")
+ax.legend()
+plt.tight_layout()
+plt.savefig(f"review-piml-gp.pdf", bbox_inches='tight', pad_inches=0)
+
+
+
+
+
+
+
+
+
+
+# #%%
+# import sys
+# import os
+# import tensorflow as tf
+# import tensorflow_probability as tfp
+
+# tfd = tfp.distributions
+# tfk = tf.keras
+# dtype = "float64"
+# tf.keras.backend.set_floatx(dtype)
+
+# sys.path.append(os.path.join("..", ".."))
+# from podnn.plotting import figsize
+# #%% Model creation
+# def prior_trainable(kernel_size, bias_size=0, dtype=None):
+#     n = kernel_size + bias_size
+#     return tfk.models.Sequential([
+#         # tfp.layers.VariableLayer(n, dtype=dtype, trainable=False),
+#         tfp.layers.VariableLayer(n, dtype=dtype, trainable=True),
+#         tfp.layers.DistributionLambda(lambda t: tfd.Independent(
+#             tfd.Normal(loc=t, scale=1),
+#             reinterpreted_batch_ndims=1,
+#         ))
+#     ])
+
+# def posterior_mean_field(kernel_size, bias_size=0, dtype=None):
+#     n = kernel_size + bias_size
+#     c = tf.math.log(tf.math.expm1(tf.constant(1., dtype=dtype)))
+#     return tfk.models.Sequential([
+#         tfp.layers.VariableLayer(2 * n, dtype=dtype),
+#         tfp.layers.DistributionLambda(lambda t: tfd.Independent(
+#             tfd.Normal(
+#                 loc=t[..., :n],
+#                 scale=1e-5 + 1. * tf.math.softplus(c + t[..., n:]),
+#             ),
+#             reinterpreted_batch_ndims=1,
+#         ))
+#     ])
+
+# model = tfk.models.Sequential([
+#     # tfk.layers.Dense(8, activation="linear"),
+#     # tfk.layers.Dense(2, activation="linear"),
+#     tfp.layers.DenseVariational(
+#         units=8,
+#         activation="relu",
+#         make_posterior_fn=posterior_mean_field,
+#         make_prior_fn=prior_trainable,
+#         kl_weight=1/N,
+#         dtype=dtype,
+#     ),
+#     tfp.layers.DenseVariational(
+#         units=2,
+#         activation="linear",
+#         make_posterior_fn=posterior_mean_field,
+#         make_prior_fn=prior_trainable,
+#         kl_weight=1/N,
+#         dtype=dtype,
+#     ),
+#     tfp.layers.DistributionLambda(lambda t:
+#         tfd.Normal(
+#             loc=t[..., :1],
+#             scale=1e-3 + tf.math.softplus(0.01 * t[..., 1:]),
+#         ),
+#         # tfd.Normal(
+#         #     loc=t,
+#         #     scale=1,
+#         # ),
+#     ),
+# ])
+# lr = 0.01
+# model.compile(loss=lambda y, model: -model.log_prob(y), optimizer=tfk.optimizers.Adam(lr))
+# epochs = 12000
+# model.fit(x, y, epochs=epochs, verbose=0)
+
+# # yhat = model(x_tst)
+# # yhats = [model(x_tst) for _ in range(100)]
+# ##%% Predictions and plotting
+
+# y_pred_list = []
+# y_pred_var_list = []
+# for i in range(200):
+#     yhat = model(x_tst)
+#     y_pred_list.append(yhat.mean().numpy())
+#     y_pred_var_list.append(yhat.variance().numpy())
+
+# u_pred = np.array(y_pred_list).mean(0)
+# u_pred_var = (np.array(y_pred_list)**2 + np.array(y_pred_var_list)).mean(0) - np.array(y_pred_list).mean(0) ** 2
+# u_pred_sig = np.sqrt(u_pred_var)
+
+# # y_preds = np.concatenate(y_pred_list, axis=1)
+# # u_pred = np.mean(y_preds, axis=1)
+# # u_pred_sig = np.std(y_preds, axis=1)
+
+# # u_pred = yhat.mean().numpy()
+# # u_pred_sig = yhat.stddev().numpy()
+# lower = u_pred - 2 * u_pred_sig
+# upper = u_pred + 2 * u_pred_sig
+
 # fig = plt.figure(figsize=figsize(1, 1, scale=2.5))
-# plt.fill_between(x_star[:, 0], lower[:, 1], upper[:, 1], 
-#                     facecolor='orange', alpha=0.5, label=r"$2\sigma_{T,hf}(x)$")
-# plt.plot(x_star, u_star[:, 1])
-# plt.plot(x_star, u_pred[:, 1], "r--")
-# plt.scatter(x_train, u_train[:, 1],)
-# plt.savefig("results/sin.pdf")
+# plt.fill_between(x_tst.ravel(), upper.ravel(), lower.ravel(), 
+#                     facecolor='C0', alpha=0.3, label=r"$3\sigma_{T}(x)$")
+# # plt.plot(x_star, u_pred_samples[:, :, 0].numpy().T, 'C0', linewidth=.5)
+# plt.plot(x_tst, u_pred, label=r"$\hat{u}_*(x)$")
+# plt.scatter(x, y, c="r", label=r"$u_T(x)$")
+# plt.plot(x_tst, y_tst, "r--", label=r"$u_*(x)$")
+# # for yhat in yhats:
+# #     plt.plot(x_tst, yhat.mean().numpy(), "b-", alpha=0.5)
+# #     plt.plot(x_tst, yhat.mean().numpy() + 2*yhat.stddev().numpy(), "b-", alpha=0.01)
+# #     plt.plot(x_tst, yhat.mean().numpy() - 2*yhat.stddev().numpy(), "b-", alpha=0.01)
+# plt.legend()
+# plt.xlabel("$x$")
+# plt.show()
+# # exit(0)
+# # # plt.savefig("results/gp.pdf")
+# # plt.savefig("results/cos.pdf")
+# # fig = plt.figure(figsize=figsize(1, 1, scale=2.5))
+# # plt.fill_between(x_star[:, 0], lower[:, 1], upper[:, 1], 
+# #                     facecolor='orange', alpha=0.5, label=r"$2\sigma_{T,hf}(x)$")
+# # plt.plot(x_star, u_star[:, 1])
+# # plt.plot(x_star, u_pred[:, 1], "r--")
+# # plt.scatter(x_train, u_train[:, 1],)
+# # plt.savefig("results/sin.pdf")
