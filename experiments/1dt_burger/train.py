21907aa63ba7d2b758ad1296967358e4d9508642
Pierre Jacquier <pierrejacquier39@gmail.com>
2020-03-23 04:56:45 -0400

    Graph updates

--- experiments/1dt_burger/train.py
+++ experiments/1dt_burger/train.py
@@ -5,6 +5,8 @@ import sys
 import os
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+from scipy.interpolate import griddata
 
 sys.path.append(os.path.join("..", ".."))
 from poduqnn.podnnmodel import PodnnModel
@@ -66,8 +68,8 @@ U_tst = model.restruct(U_tst)[0]
 U_pred = model.restruct(U_pred)[0]
 
 #%% Samples graph
-from mpl_toolkits.axes_grid1 import make_axes_locatable
-from scipy.interpolate import griddata
+hp["mu_min_out"] = [0.0005]
+hp["mu_max_out"] = [0.0105]
 n_samples = 3
 mu_lhs_in = sample_mu(n_samples, np.array(hp["mu_min"]), np.array(hp["mu_max"]))
 mu_lhs_out_min = sample_mu(n_samples, np.array(hp["mu_min_out"]), np.array(hp["mu_min"]))
@@ -79,52 +81,19 @@ n_plot_x = 2
 n_plot_y = 3
 fig = plt.figure(figsize=figsize(n_plot_x, n_plot_y, scale=2.0))
 gs = fig.add_gridspec(n_plot_x, n_plot_y)
-ax = fig.add_subplot(gs[0, :])
 x = np.linspace(hp["x_min"], hp["x_max"], hp["n_x"])
 t = np.linspace(hp["t_min"], hp["t_max"], hp["n_t"])
 xxT, ttT = np.meshgrid(x, t)
 xx, tt = xxT.T, ttT.T
 XT = np.hstack((xx.flatten()[:, None], tt.flatten()[:, None]))
 
-U_pred_grid = griddata(XT, U_pred.mean(-1).flatten(), (xx, tt), method='cubic')
-h = ax.imshow(U_pred_grid, interpolation='nearest', cmap='rainbow', 
-                extent=[t.min(), t.max(), x.min(), x.max()], 
-                origin='lower', aspect='auto')
-divider = make_axes_locatable(ax)
-cax = divider.append_axes("right", size="5%", pad=0.05)
-fig.colorbar(h, cax=cax)
-ax.set_xlabel("$t$")
-ax.set_ylabel("$x$")
-ax.set_title(r"$\hat{u_D}(\bar{s_{\textrm{tst}}})$")
-
-gs = fig.add_gridspec(n_plot_x, n_plot_y)
-ax = fig.add_subplot(gs[1, :])
-U_tst_grid = griddata(XT, U_tst.mean(-1).flatten(), (xx, tt), method='cubic')
-h = ax.imshow(U_tst_grid, interpolation='nearest', cmap='rainbow', 
-                extent=[t.min(), t.max(), x.min(), x.max()], 
-                origin='lower', aspect='auto')
-divider = make_axes_locatable(ax)
-cax = divider.append_axes("right", size="5%", pad=0.05)
-fig.colorbar(h, cax=cax)
-ax.set_xlabel("$t$")
-ax.set_ylabel("$x$")
-ax.set_title(r"$u_D(\bar{s_{\textrm{tst}}})$")
-
-plt.tight_layout()
-#plt.show()
-savefig("results/podnn-burger-graph-means")
-
-#%% Slices
+# Slices
 n_samples = 1
-n_plot_x = 2*n_samples
-times = [25, 50, 75]
-n_plot_y = len(times)
-fig = plt.figure(figsize=figsize(n_plot_x, n_plot_y, scale=2.0))
-gs = fig.add_gridspec(n_plot_x, n_plot_y)
+times = [25, 75]
 for j, time in enumerate(times):
     actual_row = 0
     for row, mu_lhs in enumerate([mu_lhs_in, mu_lhs_out]):
-        X_v_samples, _, U_samples = \
+        X_v_samples, U_samples, _ = \
             model.create_snapshots(mu_lhs.shape[0], mu_lhs.shape[0]*hp["n_t"], model.n_d, model.n_h, u, mu_lhs,
                                 t_min=hp["t_min"], t_max=hp["t_max"])
         # U_samples = np.reshape(U_samples, (hp["n_x"], hp["n_t"], -1))
@@ -135,13 +104,129 @@ for j, time in enumerate(times):
             st = hp["n_t"] * col
             en = hp["n_t"] * (col + 1)
             X_i = X_v_samples[st:en, :]
-            U_pred_i = model.restruct(model.predict(X_i))[0]
-            ax = fig.add_subplot(gs[actual_row, j])
+            U_pred_i = model.predict(X_i)
+            U_pred_i = np.reshape(U_pred_i, (hp["n_x"], hp["n_t"], -1))
+
+            if col == 0 and row == 0:
+                ax = fig.add_subplot(gs[0, 0])
+                U_tst_grid = griddata(XT, U_samples[:, :, col].flatten(), (xx, tt), method='cubic')
+                h = ax.imshow(U_tst_grid, interpolation='nearest', cmap='rainbow', 
+                                extent=[t.min(), t.max(), x.min(), x.max()], 
+                                origin='lower', aspect='auto')
+                divider = make_axes_locatable(ax)
+                cax = divider.append_axes("right", size="5%", pad=0.05)
+                fig.colorbar(h, cax=cax)
+                ax.set_xlabel("$t$")
+                ax.set_ylabel("$x$")
+                ax.axvline(X_i[times[0], 0], color="w", ls="-.")
+                ax.set_title(r"$u{\scriptsize\textrm{D}}(s=" + f"{X_i[0, 1]:.4f}" + ")$")
+
+                ax = fig.add_subplot(gs[1, 0])
+                U_pred_grid = griddata(XT, U_pred_i[:, :, 0].flatten(), (xx, tt), method='cubic')
+                h = ax.imshow(U_pred_grid, interpolation='nearest', cmap='rainbow', 
+                                extent=[t.min(), t.max(), x.min(), x.max()], 
+                                origin='lower', aspect='auto')
+                divider = make_axes_locatable(ax)
+                cax = divider.append_axes("right", size="5%", pad=0.05)
+                fig.colorbar(h, cax=cax)
+                ax.set_xlabel("$t$")
+                ax.set_ylabel("$x$")
+                ax.axvline(X_i[times[1], 0], color="w", ls="-.")
+                ax.set_title(r"$\hat{u}{\scriptsize\textrm{D}}(s=" + f"{X_i[0, 1]:.4f}" + ")$")
+
+            ax = fig.add_subplot(gs[j, actual_row + 1])
             ax.plot(x, U_pred_i[:, time, 0], "C0-", label=r"$\hat{u}_D(s_{" + lbl + r"})$")
             ax.plot(x, U_samples[:, time, col], "r--", label=r"$u_D(s_{" + lbl + r"})$")
-            ax.set_xlabel(f"$x\ (t={time})$")
+            ax.set_xlabel(f"$x$")
+            if row == 0:
+                ax.set_title(r"$s=" + f"{X_i[0, 1]:.4f}" + r" \in \Omega,\ "
+                             + f"t={X_i[time, 0]:.2f}$")
+            else:
+                ax.set_title(r"$s=" + f"{X_i[0, 1]:.4f}" + r" \in \Omega{\footnotesize\textrm{out}},\ "
+                             + f"t={X_i[time, 0]:.2f}$")
             actual_row += 1
-            if j == len(times) - 1:
+            if j == 0 and actual_row == 0:
                 ax.legend()
 plt.tight_layout()
-savefig("results/podnn-burger-graph-samples")
+savefig(os.path.join("results", "podensnn-burger-graph-meansamples"))
+
+# #%% Samples graph
+# from mpl_toolkits.axes_grid1 import make_axes_locatable
+# from scipy.interpolate import griddata
+# n_samples = 3
+# mu_lhs_in = sample_mu(n_samples, np.array(hp["mu_min"]), np.array(hp["mu_max"]))
+# mu_lhs_out_min = sample_mu(n_samples, np.array(hp["mu_min_out"]), np.array(hp["mu_min"]))
+# mu_lhs_out_max = sample_mu(n_samples, np.array(hp["mu_max"]), np.array(hp["mu_max_out"]))
+# mu_lhs_out = np.vstack((mu_lhs_out_min, mu_lhs_out_max))
+
+# #%% Contours for demo
+# n_plot_x = 2
+# n_plot_y = 3
+# fig = plt.figure(figsize=figsize(n_plot_x, n_plot_y, scale=2.0))
+# gs = fig.add_gridspec(n_plot_x, n_plot_y)
+# ax = fig.add_subplot(gs[0, :])
+# x = np.linspace(hp["x_min"], hp["x_max"], hp["n_x"])
+# t = np.linspace(hp["t_min"], hp["t_max"], hp["n_t"])
+# xxT, ttT = np.meshgrid(x, t)
+# xx, tt = xxT.T, ttT.T
+# XT = np.hstack((xx.flatten()[:, None], tt.flatten()[:, None]))
+
+# U_pred_grid = griddata(XT, U_pred.mean(-1).flatten(), (xx, tt), method='cubic')
+# h = ax.imshow(U_pred_grid, interpolation='nearest', cmap='rainbow', 
+#                 extent=[t.min(), t.max(), x.min(), x.max()], 
+#                 origin='lower', aspect='auto')
+# divider = make_axes_locatable(ax)
+# cax = divider.append_axes("right", size="5%", pad=0.05)
+# fig.colorbar(h, cax=cax)
+# ax.set_xlabel("$t$")
+# ax.set_ylabel("$x$")
+# ax.set_title(r"$\hat{u_D}(\bar{s_{\textrm{tst}}})$")
+
+# gs = fig.add_gridspec(n_plot_x, n_plot_y)
+# ax = fig.add_subplot(gs[1, :])
+# U_tst_grid = griddata(XT, U_tst.mean(-1).flatten(), (xx, tt), method='cubic')
+# h = ax.imshow(U_tst_grid, interpolation='nearest', cmap='rainbow', 
+#                 extent=[t.min(), t.max(), x.min(), x.max()], 
+#                 origin='lower', aspect='auto')
+# divider = make_axes_locatable(ax)
+# cax = divider.append_axes("right", size="5%", pad=0.05)
+# fig.colorbar(h, cax=cax)
+# ax.set_xlabel("$t$")
+# ax.set_ylabel("$x$")
+# ax.set_title(r"$u_D(\bar{s_{\textrm{tst}}})$")
+
+# plt.tight_layout()
+# #plt.show()
+# savefig("results/podnn-burger-graph-means")
+
+# #%% Slices
+# n_samples = 1
+# n_plot_x = 2*n_samples
+# times = [25, 50, 75]
+# n_plot_y = len(times)
+# fig = plt.figure(figsize=figsize(n_plot_x, n_plot_y, scale=2.0))
+# gs = fig.add_gridspec(n_plot_x, n_plot_y)
+# for j, time in enumerate(times):
+#     actual_row = 0
+#     for row, mu_lhs in enumerate([mu_lhs_in, mu_lhs_out]):
+#         X_v_samples, _, U_samples = \
+#             model.create_snapshots(mu_lhs.shape[0], mu_lhs.shape[0]*hp["n_t"], model.n_d, model.n_h, u, mu_lhs,
+#                                 t_min=hp["t_min"], t_max=hp["t_max"])
+#         # U_samples = np.reshape(U_samples, (hp["n_x"], hp["n_t"], -1))
+#         x = np.linspace(hp["x_min"], hp["x_max"], hp["n_x"])
+#         idx = np.random.choice(X_v_samples.shape[0], n_samples, replace=False)
+#         for col, idx_i in enumerate(idx):
+#             lbl = r"{\scriptscriptstyle\textrm{tst}}" if row == 0 else r"{\scriptscriptstyle\textrm{out}}"
+#             st = hp["n_t"] * col
+#             en = hp["n_t"] * (col + 1)
+#             X_i = X_v_samples[st:en, :]
+#             U_pred_i = model.restruct(model.predict(X_i))[0]
+#             ax = fig.add_subplot(gs[actual_row, j])
+#             ax.plot(x, U_pred_i[:, time, 0], "C0-", label=r"$\hat{u}_D(s_{" + lbl + r"})$")
+#             ax.plot(x, U_samples[:, time, col], "r--", label=r"$u_D(s_{" + lbl + r"})$")
+#             ax.set_xlabel(f"$x\ (t={time})$")
+#             actual_row += 1
+#             if j == len(times) - 1:
+#                 ax.legend()
+# plt.tight_layout()
+# savefig("results/podnn-burger-graph-samples")
--- experiments/2d_ackley/hyperparams.py
+++ experiments/2d_ackley/hyperparams.py
@@ -26,9 +26,9 @@ HP["train_val"] = (0.8, 0.2)
 # Deep NN hidden layers topology
 HP["h_layers"] = [64, 64]
 # Setting up TF SGD-based optimizer
-HP["epochs"] = 50000
+HP["epochs"] = 30000
 HP["lr"] = 0.003
-HP["lambda"] = 1e-4
+HP["lambda"] = 1e-2
 # Frequency of the logger
 HP["log_frequency"] = 1000
 # Non-spatial params
