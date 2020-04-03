c3ce780a2058d8326fb7ecb33fefab02415f705f
Pierre Jacquier <pierrejacquier39@gmail.com>
2020-04-03 13:33:58 -0400

    WIP burger

--- experiments/1dt_burger/hyperparams.py
+++ experiments/1dt_burger/hyperparams.py
@@ -18,7 +18,7 @@ HP["t_max"] = 5.
 HP["n_s"] = 50
 HP["n_s_tst"] = 1000
 # POD stopping param
-HP["eps"] = 1e-10
+HP["eps"] = 1e-8
 HP["eps_init"] = None
 HP["n_L"] = 0
 HP["x_noise"] = 0.
@@ -27,9 +27,9 @@ HP["train_val"] = (4/5, 1/5)
 # Deep NN hidden layers topology
 HP["h_layers"] = [140, 140]
 # Setting up TF SGD-based optimizer
-HP["epochs"] = 130000
+HP["epochs"] = 530000
 HP["lr"] = 0.001
-HP["pi_1"] = 1.5
+HP["pi_1"] = 0.5
 HP["pi_2"] = 0.1
 HP["norm"] = NORM_MEANSTD
 # Frequency of the logger
