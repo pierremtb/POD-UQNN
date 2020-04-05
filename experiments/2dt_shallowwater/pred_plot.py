79576a8e51277a1c3bb375b2dea58e72e57cd9bd
Pierre Jacquier <pierrej@gra-login3.graham.sharcnet>
2020-04-05 05:48:36 -0400

    Tweaked 2swt plot

--- experiments/2dt_shallowwater/pred_plot.py
+++ experiments/2dt_shallowwater/pred_plot.py
@@ -41,25 +41,30 @@ n_plot_x = 2*len(idx)
 n_plot_y = 5
 fig = plt.figure(figsize=figsize(n_plot_x, n_plot_y, scale=1.0))
 gs = fig.add_gridspec(n_plot_x, n_plot_y)
+ylim = (U_pred_lo[:, 0].min() - 1., U_pred_up[:, 0].max() + 1.)
 for i, t_i in enumerate(idx):
     ax = fig.add_subplot(gs[2*i:2*i+2, 0:2])
     img = plti.imread(f"cache/x_u_tst_pred.{t_i}.png")
     ax.imshow(img)
+    ax.set_xlabel(r"Water depth $\eta$")
     # ax.set_xlabel(f"$x$")
     # ax.set_ylabel(f"$y$")
     ax.set_xticks([])
     ax.set_yticks([])
     ax = fig.add_subplot(gs[2*i:2*i+2, 2:])
     lbl = r"{\scriptscriptstyle\textrm{tst},1}"
-    ax.plot(x_prime, b, "k:", label="$b$")
+    # ax.plot(x_prime, b, "k:", label="$b$")
+    ax.fill_between(x_prime, np.zeros_like(b), b,
+                    edgecolor="k", alpha=0.3, facecolor="w", hatch="/",
+                    label="$b$")
     ax.plot(x_prime, U_pred[:, i], "b-", label=r"$\hat{u}_D(s_{" + lbl + r"})$")
     ax.plot(x_prime, U_tst[:, i], "r--", label=r"$u_D(s_{" + lbl + r"})$")
     ax.fill_between(x_prime, U_pred_lo[:, i], U_pred_up[:, i],
                     alpha=0.2, label=r"$2\sigma_D(s_{" + lbl + r"})$")
     ax.set_xlabel(f"$x'$")
     ax.set_ylabel("$\eta$")
-    ax.set_ylim((U_pred_lo[:, i].min() - 1., U_pred_up[:, i].max() + 1.))
-    ax.set_title(f"$s=0.42\ m$, $t={t_i * hp['d_t']}\ s$")
+    ax.set_ylim(ylim)
+    ax.set_title(f"$s=29.368\ m$, $t={t_i * hp['d_t']}\ s$")
     if i == 0:
         ax.legend()
 plt.tight_layout()
