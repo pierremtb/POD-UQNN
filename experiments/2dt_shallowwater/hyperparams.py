839418bae63ef73d79d2cdbfdede4310c82107a9
Pierre Jacquier <pierrejacquier39@gmail.com>
2020-04-05 06:44:48 -0400

    Prepared for 2dt_sw

---
+++ experiments/2dt_shallowwater/hyperparams.py
@@ -0,0 +1,32 @@
+"""Default hyperparameters for 2D inviscid Shallow Water Equations."""
+
+from poduqnn.custombnn import NORM_MEANSTD
+
+HP = {}
+HP["mesh_idx"] = ["eta"]
+HP["mu_idx"] = [2]
+# Dimension of u(x, t, mu)
+HP["n_v"] = 1
+# Time
+HP["n_t"] = 100
+HP["d_t"] = 0.1
+HP["t_min"] = 0.
+HP["t_max"] = 99.
+# Snapshots count
+HP["n_s"] = 50
+HP["n_s_tst"] = 1
+# POD stopping param
+HP["eps"] = 1e-8
+HP["n_L"] = 0
+# Train/val split
+HP["train_val"] = (.8, .2)
+# DeepNN Topology
+HP["h_layers"] = [140, 140]
+# Setting up TF SGD-based optimizer
+HP["epochs"] = 300000
+HP["lr"] = 0.001
+HP["pi_1"] = 2.
+HP["pi_2"] = 0.1
+HP["norm"] = NORM_MEANSTD
+# Frequency of the logger
+HP["log_frequency"] = 1000
---
+++ experiments/2dt_shallowwater/train.py
@@ -0,0 +1,60 @@
+"""POD-NN modeling of a dam break, with 2D, unsteady Shallow Water Equation."""
+
+#%% Import
+import sys
+import os
+import meshio
+import pickle
+import numpy as np
+
+sys.path.append(os.path.join("..", ".."))
+from poduqnn.podnnmodel import PodnnModel
+from poduqnn.mesh import read_multi_space_sol_input_mesh
+from poduqnn.handling import split_dataset
+from poduqnn.metrics import re_s, re
+from poduqnn.plotting import savefig, figsize
+
+#%% Prepare
+from hyperparams import HP as hp
+print(hp)
+
+#%% Getting data from the files
+fake_x = np.zeros(hp["n_s"] + hp["n_s_tst"])
+test_size = hp["n_s_tst"] / (hp["n_s"] + hp["n_s_tst"])
+train_tst_idx = split_dataset(fake_x, fake_x, test_size, idx_only=True)
+with open(os.path.join("cache", "train_tst_idx.pkl"), "wb") as f:
+     pickle.dump(train_tst_idx, f)
+
+datadir = "data"
+mu_path = os.path.join(datadir, "INPUT_MONTE_CARLO.dat")
+sel = np.loadtxt(os.path.join(datadir, "sel.csv"),
+                 skiprows=1, delimiter=",")[:, 0].astype("int")
+x_mesh, connectivity, X_v, U = \
+        read_multi_space_sol_input_mesh(hp["n_s"], 1, 1, train_tst_idx[0],
+                                        hp["mesh_idx"], datadir, mu_path,
+                                        hp["mu_idx"], sel)
+
+np.save(os.path.join("cache", "x_mesh.npy"), x_mesh)
+# x_mesh = np.load(os.path.join("cache", "x_mesh.npy"))
+
+#%% Init the model
+model = PodnnModel("cache", hp["n_v"], x_mesh, hp["n_t"])
+
+
+#%% Generate the dataset from the mesh and params
+X_v_train, v_train, \
+    X_v_val, v_val, \
+    U_val = model.convert_multigpu_data(U, X_v, hp["train_val"], hp["eps"])
+# X_v_train, v_train, U_train, X_v_val, v_val, U_val = model.load_train_data()
+
+#%% Model creation
+model.initBNN(hp["h_layers"], hp["lr"], 1,
+              hp["pi_1"], hp["pi_2"], hp["norm"])
+# X_out = np.linspace(500, 1500, 300).reshape(-1, 1)
+model.train(X_v_train, v_train, X_v_val, v_val, hp["epochs"],
+            freq=hp["log_frequency"])
+
+#%%
+#v_pred, v_pred_sig = model.predict_v(X_v_val)
+## err_val = re_s(v_val.T, v_pred.T)
+## print(f"RE_v: {err_val:4f}")
