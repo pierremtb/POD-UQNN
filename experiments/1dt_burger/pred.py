ad96f3b57be46b2d69d304d222b79765c458791a
Pierre Jacquier <pierrejacquier39@gmail.com>
2020-04-15 13:37:09 -0400

    Updated pred for burger

--- experiments/1dt_burger/pred.py
+++ experiments/1dt_burger/pred.py
@@ -42,10 +42,15 @@ U_pred = model.restruct(U_pred)[0]
 hp["mu_min_out"] = [0.0005]
 hp["mu_max_out"] = [0.0105]
 n_samples = 3
-mu_lhs_in = sample_mu(n_samples, np.array(hp["mu_min"]), np.array(hp["mu_max"]))
-mu_lhs_out_min = sample_mu(n_samples, np.array(hp["mu_min_out"]), np.array(hp["mu_min"]))
-mu_lhs_out_max = sample_mu(n_samples, np.array(hp["mu_max"]), np.array(hp["mu_max_out"]))
-mu_lhs_out = np.vstack((mu_lhs_out_min, mu_lhs_out_max))
+# mu_lhs_in = sample_mu(n_samples, np.array(hp["mu_min"]), np.array(hp["mu_max"]))
+# mu_lhs_out_min = sample_mu(n_samples, np.array(hp["mu_min_out"]), np.array(hp["mu_min"]))
+# mu_lhs_out_max = sample_mu(n_samples, np.array(hp["mu_max"]), np.array(hp["mu_max_out"]))
+# mu_lhs_out = np.vstack((mu_lhs_out_min, mu_lhs_out_max))
+# np.save(os.path.join("cache", "mu_lhs_in.npy"), mu_lhs_in)
+# np.save(os.path.join("cache", "mu_lhs_out.npy"), mu_lhs_out)
+mu_lhs_in = np.load(os.path.join("cache", "mu_lhs_in.npy"))
+mu_lhs_out = np.load(os.path.join("cache", "mu_lhs_out.npy"))
+times = [25, 75]
 
 #%% Contours for demo
 n_plot_x = 2
@@ -58,70 +63,81 @@ xxT, ttT = np.meshgrid(x, t)
 xx, tt = xxT.T, ttT.T
 XT = np.hstack((xx.flatten()[:, None], tt.flatten()[:, None]))
 
+X_v_samples, _, U_samples, _ = \
+    model.create_snapshots(model.n_d, model.n_h, u, mu_lhs_in,
+                           t_min=hp["t_min"], t_max=hp["t_max"])
+X_v_samples_out, _, U_samples_out, _ = \
+    model.create_snapshots(model.n_d, model.n_h, u, mu_lhs_out,
+                           t_min=hp["t_min"], t_max=hp["t_max"])
+
+U_pred, U_pred_sig = model.predict(X_v_samples)
+U_pred_out, U_pred_sig_out = model.predict(X_v_samples_out)
+U_pred, U_pred_sig = model.restruct(U_pred), model.restruct(U_pred_sig)
+U_pred_out, U_pred_sig_out = model.restruct(U_pred_out), model.restruct(U_pred_sig_out)
+# idx = np.random.choice(X_v_samples.shape[0], n_samples, replace=False)
+idx = 0
+
+# First column: contour plot of the true value
+ax = fig.add_subplot(gs[0, 0])
+U_tst_grid = griddata(XT, U_samples[..., idx].flatten(), (xx, tt), method='cubic')
+print(U_tst_grid.shape)
+h = ax.imshow(U_tst_grid, interpolation='nearest', cmap='rainbow', 
+            extent=[t.min(), t.max(), x.min(), x.max()], 
+            origin='lower', aspect='auto')
+divider = make_axes_locatable(ax)
+cax = divider.append_axes("right", size="5%", pad=0.05)
+fig.colorbar(h, cax=cax)
+ax.set_xlabel("$t$")
+ax.set_ylabel("$x$")
+ax.axvline(X_v_samples[times[0], 0], color="w", ls="-.")
+ax.set_title(r"$u_D(s=" + f"{X_v_samples[0, 1]:.4f}" + ")$")
+
+# First column: contour plot of the predicted mean
+ax = fig.add_subplot(gs[1, 0])
+U_pred_grid = griddata(XT, U_pred[..., idx].flatten(), (xx, tt), method='cubic')
+h = ax.imshow(U_pred_grid, interpolation='nearest', cmap='rainbow', 
+            extent=[t.min(), t.max(), x.min(), x.max()], 
+            origin='lower', aspect='auto')
+divider = make_axes_locatable(ax)
+cax = divider.append_axes("right", size="5%", pad=0.05)
+fig.colorbar(h, cax=cax)
+ax.set_xlabel("$t$")
+ax.set_ylabel("$x$")
+ax.axvline(X_v_samples[times[1], 0], color="w", ls="-.")
+ax.set_title(r"$\hat{u}^\mu_D(s=" + f"{X_v_samples[0, 1]:.4f}" + ")$")
+
 # Slices
-n_samples = 1
-times = [25, 75]
-for j, time in enumerate(times):
-    actual_row = 0
-    for row, mu_lhs in enumerate([mu_lhs_in, mu_lhs_out]):
-        X_v_samples, _, U_samples, _ = \
-            model.create_snapshots(model.n_d, model.n_h, u, mu_lhs,
-                                t_min=hp["t_min"], t_max=hp["t_max"])
-        # U_samples = np.reshape(U_samples, (hp["n_x"], hp["n_t"], -1))
-        x = np.linspace(hp["x_min"], hp["x_max"], hp["n_x"])
-        idx = np.random.choice(X_v_samples.shape[0], n_samples, replace=False)
-        for col, idx_i in enumerate(idx):
-            lbl = r"{\scriptscriptstyle\textrm{tst}}" if row == 0 else r"{\scriptscriptstyle\textrm{out}}"
-            st = hp["n_t"] * col
-            en = hp["n_t"] * (col + 1)
-            X_i = X_v_samples[st:en, :]
-            U_pred_i, U_pred_i_sig = model.predict(X_i)
-            U_pred_i = np.reshape(U_pred_i, (hp["n_x"], hp["n_t"], -1))
-            U_pred_i_sig = np.reshape(U_pred_i_sig, (hp["n_x"], hp["n_t"], -1))
+def plot_slice(row, col, t, lbl, X_v, U_pred_i, U_pred_i_sig, U_true_i):
+    ax = fig.add_subplot(gs[row, col + 1])
+    ax.plot(x, U_pred_i[:, t], "b-", label=r"$\hat{u}^\mu_D(s_{" + lbl + r"})$")
+    ax.plot(x, U_true_i[:, t], "r--", label=r"$u_D(s_{" + lbl + r"})$")
+    lower = U_pred_i[:, t] - 2*U_pred_i_sig[:, t]
+    upper = U_pred_i[:, t] + 2*U_pred_i_sig[:, t]
+    ax.fill_between(x, lower, upper, alpha=0.2, label=r"$\pm2\hat{u}^\sigma_D(s_{" + lbl + r"})$")
+    ax.set_xlabel(f"$x$")
+    if row == 0:
+        ax.legend()
+    st = hp["n_t"] * col
+    en = hp["n_t"] * (col + 1)
+    X_i = X_v[st:en, :]
 
-            if col == 0 and row == 0:
-                ax = fig.add_subplot(gs[0, 0])
-                U_tst_grid = griddata(XT, U_samples[:, :, col].flatten(), (xx, tt), method='cubic')
-                h = ax.imshow(U_tst_grid, interpolation='nearest', cmap='rainbow', 
-                                extent=[t.min(), t.max(), x.min(), x.max()], 
-                                origin='lower', aspect='auto')
-                divider = make_axes_locatable(ax)
-                cax = divider.append_axes("right", size="5%", pad=0.05)
-                fig.colorbar(h, cax=cax)
-                ax.set_xlabel("$t$")
-                ax.set_ylabel("$x$")
-                ax.axvline(X_i[times[0], 0], color="w", ls="-.")
-                ax.set_title(r"$u_D(s=" + f"{X_i[0, 1]:.4f}" + ")$")
+    if row == 0:
+        ax.set_title(r"$s=" + f"{X_i[0, 1]:.4f}" + r" \in \Omega,\ "
+                        + f"t={X_i[time, 0]:.2f}$")
+    else:
+        ax.set_title(r"$s=" + f"{X_i[0, 1]:.4f}" + r" \in \Omega{\footnotesize\textrm{out}},\ "
+                        + f"t={X_i[time, 0]:.2f}$")
 
-                ax = fig.add_subplot(gs[1, 0])
-                U_pred_grid = griddata(XT, U_pred_i[:, :, 0].flatten(), (xx, tt), method='cubic')
-                h = ax.imshow(U_pred_grid, interpolation='nearest', cmap='rainbow', 
-                                extent=[t.min(), t.max(), x.min(), x.max()], 
-                                origin='lower', aspect='auto')
-                divider = make_axes_locatable(ax)
-                cax = divider.append_axes("right", size="5%", pad=0.05)
-                fig.colorbar(h, cax=cax)
-                ax.set_xlabel("$t$")
-                ax.set_ylabel("$x$")
-                ax.axvline(X_i[times[1], 0], color="w", ls="-.")
-                ax.set_title(r"$\hat{u}^\mu_D(s=" + f"{X_i[0, 1]:.4f}" + ")$")
-
-            ax = fig.add_subplot(gs[j, actual_row + 1])
-            ax.plot(x, U_pred_i[:, time, 0], "b-", label=r"$\hat{u}^\mu_D(s_{" + lbl + r"})$")
-            ax.plot(x, U_samples[:, time, col], "r--", label=r"$u_D(s_{" + lbl + r"})$")
-            lower = U_pred_i[:, time, 0] - 2*U_pred_i_sig[:, time, 0]
-            upper = U_pred_i[:, time, 0] + 2*U_pred_i_sig[:, time, 0]
-            ax.fill_between(x, lower, upper, alpha=0.2, label=r"$\pm2\hat{u}^\sigma_D(s_{" + lbl + r"})$")
-            ax.set_xlabel(f"$x$")
-            if row == 0:
-                ax.set_title(r"$s=" + f"{X_i[0, 1]:.4f}" + r" \in \Omega,\ "
-                             + f"t={X_i[time, 0]:.2f}$")
-            else:
-                ax.set_title(r"$s=" + f"{X_i[0, 1]:.4f}" + r" \in \Omega{\footnotesize\textrm{out}},\ "
-                             + f"t={X_i[time, 0]:.2f}$")
-            actual_row += 1
-            if j == 0:
-                ax.legend()
+for row, time in enumerate(times):
+    lbl = r"{\scriptscriptstyle\textrm{tst}}"
+    X_v = X_v_samples
+    U_pred_i, U_pred_i_sig = U_pred[0, :, :, idx], U_pred_sig[0, :, :, idx]
+    U_true_i = U_samples[:, :, idx]
+    plot_slice(row, 0, time, lbl, X_v, U_pred_i, U_pred_i_sig, U_true_i)
+    lbl = r"{\scriptscriptstyle\textrm{out}}"
+    X_v = X_v_samples_out
+    U_pred_i, U_pred_i_sig = U_pred_out[0, :, :, idx], U_pred_sig_out[0, :, :, idx]
+    U_true_i = U_samples_out[:, :, idx]
+    plot_slice(row, 1, time, lbl, X_v, U_pred_i, U_pred_i_sig, U_true_i)
 plt.tight_layout()
-
 savefig("results/podensnn-burger-graph-meansamples")
--- poduqnn/podnnmodel.py
+++ poduqnn/podnnmodel.py
@@ -4,6 +4,7 @@ import os
 import pickle
 import time
 import tensorflow as tf
+import tensorflow_probability as tfp
 import numpy as np
 from tqdm import tqdm
 import numba as nb
@@ -340,7 +341,7 @@ class PodnnModel:
                      / (samples * (samples - 1)))
         return U_pred, U_pred_sig
 
-    def predict(self, X_v):
+    def predict_mc(self, X_v):
         """Predict the expanded solution."""
         n_M = len(self.regnn)
         U_pred_samples = np.zeros((self.n_h, X_v.shape[0], n_M))
@@ -361,6 +362,22 @@ class PodnnModel:
             U_pred_sig += self.pod_sig[:, np.newaxis]
 
         return U_pred.astype(self.dtype), U_pred_sig.astype(self.dtype)
+
+    def predict(self, X_v, samples=100):
+        print(f"Averaging {samples} model configurations...")
+        v_pred, v_pred_sig = self.predict_v(X_v)
+        v_dist = tfp.distributions.Normal(loc=v_pred, scale=v_pred_sig)
+        U_sum = np.zeros((self.n_h, X_v.shape[0]))
+        U_sum_sq = np.zeros_like(U_sum)
+        for i in tqdm(range(samples)):
+            U_i = self.project_to_U(v_dist.sample().numpy())
+            U_sum += U_i
+            U_sum_sq += U_i**2
+        U_pred = U_sum / samples
+        U_pred_sig = np.sqrt((samples * U_sum_sq - U_sum**2) \
+                     / (samples * (samples - 1)))
+        return U_pred, U_pred_sig
+
 
     def restruct(self, U, no_s=False, n_t=None):
         """Restruct the snapshots matrix DOFs/space-wise and time/snapshots-wise."""
