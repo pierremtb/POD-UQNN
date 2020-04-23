9f13dac624304b28d4e0a8a74e84d7eb0e48a329
Pierre Jacquier <pierrejacquier39@gmail.com>
2020-04-23 19:57:55 -0400

    Better results stacking up 40 layers

--- experiments/1dt_burger/hyperparams.py
+++ experiments/1dt_burger/hyperparams.py
@@ -19,24 +19,24 @@ HP["t_max"] = 5.
 HP["n_s"] = 50
 HP["n_s_tst"] = 100
 # POD stopping param
-HP["eps"] = 1e-3
-HP["eps_init"] = 1e-3
+HP["eps"] = 1e-4
+HP["eps_init"] = 1e-4
 HP["n_L"] = 0
 HP["x_noise"] = 0.
 # Train/val split
 HP["train_val"] = (4/5, 1/5)
 # Deep NN hidden layers topology
-HP["h_layers"] = [40, 40]
+HP["h_layers"] = [40, 40, 40]
 # Setting up TF SGD-based optimizer
-HP["epochs"] = 150000
-HP["lr"] = 0.01
+HP["epochs"] = 100000
+HP["lr"] = 0.001
 HP["exact_kl"] = False
 HP["activation"] = "tanh"
 HP["pi_0"] = 0.5
-HP["pi_1"] = 2.
+HP["pi_1"] = 1.
 HP["pi_2"] = 0.1
 HP["soft_0"] = 0.01
-HP["adv_eps"] = 0.001
+HP["adv_eps"] = 0.01
 HP["norm"] = NORM_MEANSTD
 # Frequency of the logger
 HP["log_frequency"] = 500
