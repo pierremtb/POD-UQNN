f7a182bca6086d8ff6f8a80433456a60e7b685ae
Pierre Jacquier <pierrejacquier39@gmail.com>
2020-05-24 17:31:40 -0400

    Updated hps

--- experiments/2d_ackley/hyperparams.py
+++ experiments/2d_ackley/hyperparams.py
@@ -22,19 +22,19 @@ HP["n_t"] = 0
 HP["n_s"] = 500
 HP["n_s_tst"] = 300
 # POD stopping param
-HP["eps"] = 1e-4
+HP["eps"] = 1e-10
 HP["n_L"] = 0
 # Train/val split
 HP["train_val"] = (0.8, 0.2)
 # Deep NN hidden layers topology
-HP["h_layers"] = [128, 128]
+HP["h_layers"] = [128, 128, 128]
 # Setting up TF SGD-based optimizer
 HP["n_M"] = 5
-HP["epochs"] = 35000
-HP["lr"] = 0.003
+HP["epochs"] = 50000
+HP["lr"] = 0.001
 HP["lambda"] = 0.001
-HP["adv_eps"] = 0.
-HP["soft_0"] = 1.
+HP["adv_eps"] = 0
+HP["soft_0"] = 0.01
 HP["norm"] = NORM_MEANSTD
 # Frequency of the logger
 HP["log_frequency"] = 2000
